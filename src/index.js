46bba09254ce79767029c5b33ee6d8f01cdfcecf
mokshdev <mokshsingh12345@gmail.com>
2025-03-16 23:51:24 +0530

    minor change

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ function walkDog() {
 
     return new Promise((resolve,rejct)=>{
         setTimeout(()=>{
-            resolve("you  walk the dog");
+            const dogWalked = false
+
+            if (dogWalked) {
+                resolve("you  walk the dog");
+            } else {
+                rejct("you didnt walked the dog")
+            }
+
+
+            
             
         }, 1500)
     })
@@ -35,15 +44,16 @@ function throwTrash() {
 }
 
 
-walkDog()
-    .then(value => {
-        console.log(value)
-        return cleanKitchen()
-    })
-    .then(value => {
-        console.log(value)
-        return throwTrash()
-    })
-    .then(value => 
-        console.log(value)
-    )
+// walkDog()
+//     .then(value => {
+//         console.log(value)
+//         return cleanKitchen()
+//     })
+//     .then(value => {
+//         console.log(value)
+//         return throwTrash()
+//     })
+//     .then(value => 
+//         console.log(value)
+//     )
+walkDog().then((message) => console.log(message))
